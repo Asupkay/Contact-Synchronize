fcd6bec289e06b164350e8cbc362af7a6da60a25
Alex Supkay <asupkay1124@gmail.com>
2018-12-30 20:19:54 -0500

    add spaghett

--- RandomMeme/background.js
+++ RandomMeme/background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener((tab) => {
     let audioPlayer = new Audio();
-    const audioClips = ["depression.mp3", "bruh.mp3"];
+    const audioClips = ["depression.mp3", "bruh.mp3", "spaghett.mp3"];
     const randomClipNum = Math.floor(Math.random() * audioClips.length);
     audioPlayer.src = audioClips[randomClipNum];
     audioPlayer.play();
